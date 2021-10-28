6495b81d22f99ca362a1245437db2a60affd2554
Жанболат Инабат Нурланкызы <@lcoast.itstep>
2021-10-28 20:54:10 +0600

    now you can see the proj

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let productsArray = JSON.parse(localStorage.getItem("productsArray"))
 console.log(productsArray)
+listDiv = $("#list")
+for (let i = 0; i < productsArray.length; i++) {
+    listDiv.append(` <div class="info_block">
+
+<p> Название вашего товара: ${productsArray[i].name}</p>
+<p> Цена товара: ${productsArray[i].price}</p>
+        </div>`)
+}
